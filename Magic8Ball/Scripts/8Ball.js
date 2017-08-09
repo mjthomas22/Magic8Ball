6297fe0c340b8d40187104da4539f940ce788f21
mjthomas22 <mthomas22@gmail.com>
2017-08-09 09:48:43 -0400

    add shake

---
+++ Magic8Ball/Scripts/8Ball.js
@@ -0,0 +1,11 @@
+﻿var fortune = ["Better Luck Next Time", "Your going to have a good day", "Your house will catch on fire", "Look behind you", "You will find a bag of money",
+    "Git init"];
+
+function GetRando() {
+    var num = Math.round(Math.random() * (fortune.length - 1));
+    return num;
+}
+
+function GetFortune() {
+    document.getElementById("fortune").innerHTML = fortune[GetRando()];
+}
